42b5e8d1d26a4ff0b15191c5f0384486e1fa1cce
Paul Nettleton <k9@k9withabone.dev>
2023-04-21 18:04:30 -0500

    feat: check for existing systemd service

---
+++ src/cli/systemd_dbus.rs
@@ -0,0 +1,40 @@
+use nix::unistd::Uid;
+use zbus::{blocking::Connection, dbus_proxy};
+
+pub fn unit_files() -> zbus::Result<impl Iterator<Item = UnitFile>> {
+    let connection = Connection::system()?;
+    let manager = ManagerProxyBlocking::new(&connection)?;
+    let mut unit_files = manager.list_unit_files()?;
+
+    if !Uid::current().is_root() {
+        let connection = Connection::session()?;
+        let manager = ManagerProxyBlocking::new(&connection)?;
+        unit_files.extend(manager.list_unit_files()?);
+    }
+
+    Ok(unit_files.into_iter().map(Into::into))
+}
+
+#[dbus_proxy(
+    interface = "org.freedesktop.systemd1.Manager",
+    default_service = "org.freedesktop.systemd1",
+    default_path = "/org/freedesktop/systemd1"
+)]
+trait Manager {
+    fn list_unit_files(&self) -> zbus::Result<Vec<(String, String)>>;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct UnitFile {
+    pub file_name: String,
+    pub status: String,
+}
+
+impl From<(String, String)> for UnitFile {
+    fn from(value: (String, String)) -> Self {
+        Self {
+            file_name: value.0,
+            status: value.1,
+        }
+    }
+}
