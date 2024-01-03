e48e4a925d705514710a76822062bc6f2fb97eed
Paul Nettleton <k9@k9withabone.dev>
2024-01-03 10:26:05 -0600

    feat: set compatibility with `--podman-version`
    Setting a lower podman version will downgrade the quadlet's
    compatibility. Will return an error if an incompatible quadlet option is
    used.
    
    Implemented as an in-place, one-way transformation of `quadlet::File`
    and its parts. That way, there is less complexity than dealing with
    multiple versions of each quadlet resource type.
    
    `--podman-version` has aliases `--compatibility` and `--compat`.
    
    Also enabled `wrap_help` feature of clap.
    
    Closes: #45

--- src/quadlet/kube.rs
+++ src/quadlet/kube.rs
@@ -5,6 +5,8 @@ use std::{
 
 use serde::{Serialize, Serializer};
 
+use super::{DowngradeError, PodmanVersion};
+
 #[derive(Serialize, Debug, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Kube {
@@ -49,6 +51,56 @@ impl Kube {
             yaml,
         }
     }
+
+    /// Downgrade compatibility to `version`.
+    ///
+    /// This is a one-way transformation, calling downgrade a second time with a higher version
+    /// will not increase the quadlet options used.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if a used quadlet option is incompatible with the given [`PodmanVersion`].
+    pub fn downgrade(&mut self, version: PodmanVersion) -> Result<(), DowngradeError> {
+        if version < PodmanVersion::V4_7 {
+            for auto_update in std::mem::take(&mut self.auto_update) {
+                self.push_arg("annotation", &auto_update.to_annotation());
+            }
+        }
+
+        if version < PodmanVersion::V4_6 {
+            if let Some(podman_args) = self.podman_args.take() {
+                return Err(DowngradeError {
+                    quadlet_option: String::from("PodmanArgs"),
+                    value: podman_args,
+                    supported_version: PodmanVersion::V4_6,
+                });
+            }
+        }
+
+        if version < PodmanVersion::V4_5 {
+            if let Some(log_driver) = self.log_driver.take() {
+                return Err(DowngradeError {
+                    quadlet_option: String::from("LogDriver"),
+                    value: log_driver,
+                    supported_version: PodmanVersion::V4_5,
+                });
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Add `--{flag} {arg}` to `PodmanArgs=`.
+    fn push_arg(&mut self, flag: &str, arg: &str) {
+        let podman_args = self.podman_args.get_or_insert_with(String::new);
+        if !podman_args.is_empty() {
+            podman_args.push(' ');
+        }
+        podman_args.push_str("--");
+        podman_args.push_str(flag);
+        podman_args.push(' ');
+        podman_args.push_str(arg);
+    }
 }
 
 impl Display for Kube {
@@ -69,6 +121,11 @@ pub enum AutoUpdate {
 }
 
 impl AutoUpdate {
+    /// Podman-specific annotation for `podman auto-update`.
+    ///
+    /// See <https://docs.podman.io/en/stable/markdown/podman-auto-update.1.html>
+    const ANNOTATION_KEY: &'static str = super::AutoUpdate::LABEL_KEY;
+
     /// Extracts all valid values of the `io.containers.autoupdate` annotation from `annotations`,
     /// the last value of which is parsed into an [`AutoUpdate`].
     ///
@@ -82,7 +139,7 @@ impl AutoUpdate {
             // or `io.containers.autoupdate/$container=[registry|local]`
             // see https://docs.podman.io/en/stable/markdown/podman-auto-update.1.html#auto-updates-and-kubernetes-yaml
             annotation
-                .strip_prefix("io.containers.autoupdate")
+                .strip_prefix(Self::ANNOTATION_KEY)
                 .and_then(|auto_update| {
                     let (container, auto_update) = auto_update.split_once('=')?;
                     let auto_update = auto_update.parse().ok()?;
@@ -101,6 +158,16 @@ impl AutoUpdate {
         });
 
         auto_updates
+    }
+
+    fn to_annotation(&self) -> String {
+        match self {
+            Self::All(auto_update) => format!("{}={auto_update}", Self::ANNOTATION_KEY),
+            Self::Container {
+                container,
+                auto_update,
+            } => format!("{}/{container}={auto_update}", Self::ANNOTATION_KEY),
+        }
     }
 }
 
